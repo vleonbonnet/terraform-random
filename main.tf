bb8c53261dba9fef97873c12ad61d41f4616f635
Valentin Leon <Valentin.Leon@nccgroup.trust>
2019-07-12 10:48:04 -0700

    local exec

--- main.tf
+++ main.tf
@@ -7,6 +7,12 @@ resource "random_id" "random" {
   byte_length = 16
 }
 
+resource "null_resource" "cmd1" {
+  provisioner "local-exec" {
+      command = "sudo whoami"
+        }
+        }
+
 output "random" {
   value = "${random_id.random.hex}"
 }

